10e7a43e4c162ac191095b66a2b7d52c39880ea3
Asad <muhammadasaddun@gmail.com>
2025-09-01 08:55:25 +0700

    file error 503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ICON</title>
+    <link rel="icon" href="{{ asset('icon.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app-1755576892.css') }}">
+</head>
+
+<body class="bg-light d-flex flex-column" style="min-height: 100vh; padding-bottom: 100px;">
+    <div class="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
+        <h2>503</h2>
+        <h3>Server Bermasalah atau Sedang Maintenance</h3>
+        <p>Terima kasih atas pengertian Anda.</p>
+        <p>Silahkan hubungi ICT jika ada yang ingin ditanyakan.</p>
+    </div>
+</body>
+
+</html>
