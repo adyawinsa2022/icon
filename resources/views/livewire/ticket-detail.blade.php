66477a9bd4e38be0981b6e1b4b40361835958528
Asad <muhammadasaddun@gmail.com>
2026-05-14 21:34:06 +0700

    fix: repair display ticket description

--- resources/views/livewire/ticket-detail.blade.php
+++ resources/views/livewire/ticket-detail.blade.php
@@ -47,7 +47,7 @@
                 </tbody>
             </table>
 
-            <p class="mt-3">{!! html_entity_decode($ticket['content'] ?? '-') !!}</p>
+            <p class="mt-3">{!! nl2br(e($ticket['content'] ?? '-')) !!}</p>
 
             @if ($userProfile != 'User' && !in_array($userName, $assignedTechs) && $ticket['status'] < 5)
                 <div class="d-flex justify-content-center">
